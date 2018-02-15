809a1a2b4f4081663d501370a1decb5f952a5a0a
mfrasca <mario@anche.no>
2018-02-14 21:03:43 -0500

    added OSM copyright, cleaning up code.

--- app/src/main/java/me/ghini/tour/FullscreenActivity.java
+++ app/src/main/java/me/ghini/tour/FullscreenActivity.java
@@ -1,21 +1,15 @@
 package me.ghini.tour;
 
-import android.annotation.SuppressLint;
 import android.content.Context;
-import android.content.res.AssetManager;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.graphics.drawable.Icon;
 import android.preference.PreferenceManager;
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.os.Handler;
 import android.util.DisplayMetrics;
-import android.view.MotionEvent;
-import android.view.View;
+import android.widget.TextView;
 
 import com.readystatesoftware.sqliteasset.SQLiteAssetHelper;
 
@@ -24,6 +18,7 @@ import org.osmdroid.config.Configuration;
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
 import org.osmdroid.util.GeoPoint;
 import org.osmdroid.views.MapView;
+import org.osmdroid.views.overlay.CopyrightOverlay;
 import org.osmdroid.views.overlay.ItemizedIconOverlay;
 import org.osmdroid.views.overlay.ItemizedOverlayWithFocus;
 import org.osmdroid.views.overlay.OverlayItem;
@@ -31,8 +26,6 @@ import org.osmdroid.views.overlay.ScaleBarOverlay;
 import org.osmdroid.views.overlay.mylocation.GpsMyLocationProvider;
 import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay;
 
-import java.io.IOException;
-import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -42,8 +35,6 @@ import java.util.List;
  */
 public class FullscreenActivity extends AppCompatActivity {
     MapView map = null;
-    private ScaleBarOverlay mScaleBarOverlay;
-    private MyLocationNewOverlay mLocationOverlay;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -53,49 +44,60 @@ public class FullscreenActivity extends AppCompatActivity {
         Configuration.getInstance().load(context, PreferenceManager.getDefaultSharedPreferences(context));
         setContentView(R.layout.activity_fullscreen);
 
-        map = (MapView) findViewById(R.id.map);
+        map = findViewById(R.id.map);
         map.setBuiltInZoomControls(true);
         map.setMultiTouchControls(true);
         map.setTileSource(TileSourceFactory.MAPNIK);
+
         // my location position - it's an overlay
-        mLocationOverlay = new MyLocationNewOverlay(new GpsMyLocationProvider(context), map);
+        MyLocationNewOverlay locationOverlay = new MyLocationNewOverlay(new GpsMyLocationProvider(context), map);
         Bitmap icon = BitmapFactory.decodeResource(context.getResources(),
                 R.drawable.ic_location_dot);
-        mLocationOverlay.setPersonIcon(icon);
-        mLocationOverlay.setPersonHotspot(icon.getHeight()/2,icon.getHeight()/2);
-        mLocationOverlay.enableMyLocation();
+        locationOverlay.setPersonIcon(icon);
+        locationOverlay.setPersonHotspot(icon.getHeight()/2,icon.getHeight()/2);
+        locationOverlay.enableMyLocation();
+        map.getOverlays().add(locationOverlay);
+
         // POIs is an other overlay - points should come from a database
         TaxonomyDatabase db = new TaxonomyDatabase(context);
 
         List<OverlayItem> items = db.getPOIs();
 
-        //the overlay
-        ItemizedOverlayWithFocus<OverlayItem> mOverlay = new ItemizedOverlayWithFocus<OverlayItem>(context, items,
+        // the POI overlay
+        ItemizedOverlayWithFocus<OverlayItem> POIOverlay = new ItemizedOverlayWithFocus<OverlayItem>(context, items,
                 new ItemizedIconOverlay.OnItemGestureListener<OverlayItem>() {
                     @Override
                     public boolean onItemSingleTapUp(final int index, final OverlayItem item) {
                         //do something
+                        TextView text = findViewById(R.id.text);
+                        text.setText(item.getTitle());
                         return false;
                     }
+
                     @Override
                     public boolean onItemLongPress(final int index, final OverlayItem item) {
                         return false;
                     }
                 });
-        mOverlay.setFocusItemsOnTap(true);
-        map.getOverlays().add(mOverlay);
+        POIOverlay.setFocusItemsOnTap(false);
+        map.getOverlays().add(POIOverlay);
 
-        map.getOverlays().add(mLocationOverlay);
-        mScaleBarOverlay = new ScaleBarOverlay(map);
-        mScaleBarOverlay.setCentred(true);
+        /* the initial centre point */
         IMapController mapController = map.getController();
         mapController.setZoom(5);
         GeoPoint startPoint = new GeoPoint(5.5, -74.5);
         mapController.setCenter(startPoint);
+
+        /* the scale bar */
+        ScaleBarOverlay scaleBarOverlay = new ScaleBarOverlay(map);
+        scaleBarOverlay.setCentred(true);
         DisplayMetrics dm = getResources().getDisplayMetrics();
-//play around with these values to get the location on screen in the right place for your applicatio
-        mScaleBarOverlay.setScaleBarOffset(dm.widthPixels / 2, 10);
-        map.getOverlays().add(this.mScaleBarOverlay);
+        // play around with these values to get the location on
+        // screen in the right place for your application
+        scaleBarOverlay.setScaleBarOffset(dm.widthPixels / 2, 10);
+        map.getOverlays().add(scaleBarOverlay);
+
+        map.getOverlays().add(new CopyrightOverlay(context));
     }
 
     public void onResume(){
@@ -111,27 +113,12 @@ public class FullscreenActivity extends AppCompatActivity {
         super.onPause();
     }
 
-    static Bitmap getBitmapFromAsset(Context context, String filePath) {
-        AssetManager assetManager = context.getAssets();
-
-        InputStream stream;
-        Bitmap bitmap = null;
-        try {
-            stream = assetManager.open(filePath);
-            bitmap = BitmapFactory.decodeStream(stream);
-            stream.close();
-        } catch (IOException e) {
-            // handle exception
-        }
-
-        return bitmap;
-    }
 }
 
 class TaxonomyDatabase extends SQLiteAssetHelper {
 
     private static final String DATABASE_NAME = "poi.db";
-    private static final int DATABASE_VERSION = 1;
+    private static final int DATABASE_VERSION = 4;
 
     TaxonomyDatabase(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
